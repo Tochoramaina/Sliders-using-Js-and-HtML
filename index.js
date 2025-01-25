2f3f4bd1ec9ba62ef9071e304407e0c43ed7bc25
Samwel Tochora <samtochora2001@gmail.com>
2025-01-25 10:56:32 +0300

    adding details functionality to see more button

--- index.js
+++ index.js
@@ -12,7 +12,13 @@
     showSlider('prev')
  };
 
+ // a timeout function to allow allow user to click buttons after 2ms
+ let unAcceptClick;
  const showSlider = (type) => {
+    // restricting users clicking the button when they are running
+    nextButton.style.pointerEvents = 'none';
+    prevButton.style.pointerEvents = 'none';
+    carousel.classList.remove('prev', 'next')
     let items = document.querySelectorAll('.carousel .list .item')
     if(type === 'next'){
         listHtml.appendChild(items[0]);
@@ -25,4 +31,19 @@
         // adding prev animation effects
         carousel.classList.add("prev")
     }
+    // clearing time to ensure the animation time restart from the beggining instead of cumulatively
+    clearTimeout(unAcceptClick)
+    unAcceptClick = setTimeout(() => {
+        nextButton.style.pointerEvents = 'auto';
+        prevButton.style.pointerEvents = 'auto';
+    }, 2000)
+ }
+
+ seeMoreButton.forEach(button => {
+    button.onclick = function(){
+        carousel.classList.add('showDetail')
+    }
+ })
+ backButton.onclick = function(){
+    carousel.classList.remove('showDetail')
  }
