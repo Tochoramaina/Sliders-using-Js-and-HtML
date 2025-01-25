abbae78668a266f1df611c296d25d06435cc3b71
Samwel Tochora <samtochora2001@gmail.com>
2025-01-25 09:24:15 +0300

    adding next and prev button functionality plus animation effects

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
- 
+ let nextButton = document.getElementById('next')
+ let prevButton = document.getElementById("prev")
+ let backButton = document.getElementById('back')
+ let seeMoreButton = document.querySelectorAll(".seeMore");
+ let carousel = document.querySelector('.carousel');
+ let listHtml = document.querySelector(".carousel .list")
+
+ nextButton.onclick = function(){
+    showSlider('next')
+ };
+ prevButton.onclick = function(){
+    showSlider('prev')
+ };
+
+ const showSlider = (type) => {
+    let items = document.querySelectorAll('.carousel .list .item')
+    if(type === 'next'){
+        listHtml.appendChild(items[0]);
+        //adding next animation effects
+        carousel.classList.add('next')
+    }
+    else{
+        let positionLast = items.length - 1;
+        listHtml.prepend(items[positionLast])
+        // adding prev animation effects
+        carousel.classList.add("prev")
+    }
+ }
